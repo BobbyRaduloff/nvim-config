2245d4ef888e5bd2fb5d1d11797c5dad70a79486
Boris Radulov <boris@cbt.bg>
2024-02-14 17:59:13 +0200

    initial config

---
+++ lua/custom/configs/conform.lua
@@ -0,0 +1,30 @@
+--type conform.options
+local options = {
+  lsp_fallback = true,
+
+  formatters_by_ft = {
+    lua = { "stylua" },
+    javascript = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    typescript = { "prettier" },
+    json = { "prettier" },
+    jsx = { "prettier" },
+    tsx = { "prettier" },
+    sh = { "shfmt" },
+    javascriptreact = { "prettier" },
+    typescriptreact = { "prettier" },
+    prisma = { "prisma-fmt" },
+  },
+
+  -- adding same formatter for multiple filetypes can look too much work for some
+  -- instead of the above code you could just use a loop! the config is just a table after all!
+
+  format_on_save = {
+    --   -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+require("conform").setup(options)
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,82 @@
+local overrides = require "custom.configs.overrides"
+
+---@type NvPluginSpec[]
+local plugins = {
+
+  -- Override plugin definition options
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+
+  -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  -- Install a plugin
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    --  for users those who want auto-save conform + lazyloading!
+    -- event = "BufWritePre"
+    config = function()
+      require "custom.configs.conform"
+    end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = overrides.copilot,
+    config = function()
+      require("copilot").setup {}
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
+    },
+    opts = {
+      sources = {
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "copilot", group_index = 2 },
+        { name = "luasnip", group_index = 2 },
+        { name = "buffer", group_index = 2 },
+        { name = "nvim_lua", group_index = 2 },
+        { name = "path", group_index = 2 },
+      },
+    },
+    enabled = true,
+  },
+}
+
+return plugins
