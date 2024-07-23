6fbb58db085e83711b6433df025748f979e322f4
Boris Radulov <boris@cbt.bg>
2024-07-23 11:22:29 +0300

    lsps

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,7 +4,8 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "prismals", "rust_analyzer", "omnisharp" }
+local servers =
+  { "html", "cssls", "tsserver", "clangd", "prismals", "rust_analyzer", "omnisharp", "tailwindcss", "gopls", "astro" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -88,11 +88,40 @@ local plugins = {
       require("spectre").setup()
     end,
   },
+
+  -- {
+  --   "luckasRanarison/tailwind-tools.nvim",
+  --   lazy = false,
+  --   dependencies = { "nvim-treesitter/nvim-treesitter" },
+  --   opts = {
+  --     document_color = {
+  --       kind = "foreground",
+  --     },
+  --   }, -- your configuration
+  -- },
+
   {
-    "laytan/tailwind-sorter.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
-    build = "cd formatter && npm ci && npm run build",
-    config = true,
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
   },
 }
 
